33ac70546e8035b4c603ae4890eeb683b2f3da3c
reem <janinareem@gmail.com>
2022-06-02 22:38:06 +0200

    Add create method

--- app/controllers/v1/houses_controller.rb
+++ app/controllers/v1/houses_controller.rb
@@ -8,9 +8,22 @@ class V1::HousesController < ApplicationController
     render json: House.find(params[:id])
   end
 
-  def create; end
+  def create
+    @house = House.create(house_params)
+
+    if @house.save
+      render json: { status: 200, message: 'new house created successfully' }
+    else
+      render json: { status: 400, message: 'can not be created!' }
+    end
+  end
 
   def update; end
 
   def destroy; end
+
+  private
+  def house_params
+    params.require(:house).permit(:address, :city, :zip_code, :image, :bathrooms, :rooms, :surroundings, :price, :construction_year)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
 user1 = User.create(username: 'sahar')
-house1 = House.create(id: 1, user_id: 1, address: 'st 33/10', city: 'alexandria', zip_code: 1245, image: 'none', 
-  bathrooms: 12, rooms: 15, surroundings: 'sea', price: 250, construction_year: 1994)
+house1 = House.create(id: 1, user_id: 1, address: 'st 33/10', city: 'alexandria', zip_code: 1245, image: 'none', bathrooms: 12, rooms: 15, surroundings: 'sea', price: 250, construction_year: 1994)
 house2 = House.create(id: 2, user_id: 1, address: 'st 30/10', city: 'beirut', zip_code: 1101,
   image: 'none', bathrooms: 2, rooms: 1, surroundings: 'city', price: 250, construction_year: 2004)
