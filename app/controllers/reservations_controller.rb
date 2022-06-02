572c4bfe19dc16fcb884da7dec2ac83265932778
German Renato Cobian <germancobian@hotmail.com>
2022-06-02 15:30:14 -0700

    Implement create and destroy actions for reservations controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -11,4 +11,29 @@ class ReservationsController < ApplicationController
       render json: reservation.to_json
     end
   end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    
+    if @reservation.save
+      render json: @reservation.to_json, status: 201
+    else
+      render status: 500, json: { error: 'Reservation could not be created' }.to_json
+  end
+
+  def destroy
+    @reservation = Reservation.destroy_by(id: params[:id])
+    
+    if @reservation.destroyed?
+      render status: 200, json: { message: 'Reservation destroyed' }.to_json
+    else
+      render status: 500, json: { error: 'Reservation could not be destroyed' }.to_json
+  end
+
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:date, :user_id, :house_id)
+  end
 end
