d82673c7bc17b1c36b8e58caa82db18cb96f069f
Aime Malaika <aimemalaika1995@gmail.com>
2022-06-10 16:34:45 +0200

    tested reservation

--- spec/requests/v1/user/sessions_request_spec.rb
+++ spec/requests/v1/user/sessions_request_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Users::SessionsController, type: :request do
-  describe 'POST /v1/users/sessions' do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+  describe 'POST /v1/login' do
+    
 end
