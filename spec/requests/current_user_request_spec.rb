f2452a80cd13beedf39a7e91dc98c59b9496133f
Aime Malaika <aimemalaika1995@gmail.com>
2022-06-10 16:48:25 +0200

    Test user signup

--- spec/requests/current_user_request_spec.rb
+++ spec/requests/current_user_request_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'CurrentUsers', type: :request do
+  before(:all) do
+    @user = FactoryBot.create(:user)
+    post '/v1/login', params: { user: { email: @user.email , password: @user.password } }, as: :json
+    @authorization = response.header['Authorization']
+  end
   describe 'GET /v1/current_user' do
-    pending "add some examples to (or delete) #{__FILE__}"
+    it 'returns the current user' do
+      get '/v1/current_user', headers: { 'Authorization': @authorization }
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns an error if the user is not logged in' do
+      get '/v1/current_user'
+      expect(response).to have_http_status(:unauthorized)
+    end
   end
 end
--- spec/requests/v1/user/registrations_request_spec.rb
+++ spec/requests/v1/user/registrations_request_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Users::RegistrationsController, type: :request do
-  describe 'POST /v1/users/registrations' do
-    pending "add some examples to (or delete) #{__FILE__}"
+  describe 'POST v1/signup' do
+    it 'creates a new user' do
+      post '/v1/signup', params: { user: { username: 'test', email: 'aaaa@test.com', password: '123456', password_confirmation: '123456' } }, as: :json
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
